bc05775df757c38b4e39feb5e3687fe4cc2d9de5
patel vraj <122253235+vrajpatel0007@users.noreply.github.com>
2024-10-01 13:03:11 +0530

    routes

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ import authRoutes from "./src/routes/auth.route.js";
 import { ENV_VARS } from "./src/config/envVars.js";
 import { connectDB } from "./src/config/db.js";
 import movieRoutes from "./src/routes/movie.route.js"
+import searchRoutes from "./src/routes/search.route.js"
+import tvRoutes from "./src/routes/tv.route.js"
 const app = express();
 const PORT = ENV_VARS.PORT
 const __dirname = path.resolve();
@@ -14,6 +16,8 @@ app.use(cookieParser());
 
 app.use("/api/v1/auth", authRoutes)
 app.use("/api/v1/movie", movieRoutes);
+app.use("/api/v1/search", searchRoutes );
+app.use("/api/v1/tv", tvRoutes );
 
 app.listen(PORT, () => {
     console.log(`Server started at http://localhost:${PORT}`);
